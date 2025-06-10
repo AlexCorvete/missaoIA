11dcd051eb85c0083f4ccf368ed62ace10627c40
Alex Pontes <alexsandro.pontes@escola.pr.gov.br>
2025-06-10 09:16:48 -0300

    mais uma correção

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const perguntas = [
     {
         enunciado: 
         "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
+        alternativas: ["Isso é assustador!", "Isso é maravilhoso!"]
     },
     {
         enunciado: 
@@ -16,7 +16,7 @@ const perguntas = [
         alternativas: [
             "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
-        ],
+        ]
     },
     {
         enunciado: 
@@ -24,7 +24,7 @@ const perguntas = [
         alternativas: [
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores."
-        ],
+        ]
     },
     {
         enunciado: 
@@ -32,7 +32,7 @@ const perguntas = [
         alternativas: [
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
             "Criar uma imagem utilizando um gerador de imagem de IA."
-        ],
+        ]
     },
     {
         enunciado: 
@@ -40,7 +40,7 @@ const perguntas = [
         alternativas: [
             "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-        ],
+        ]
     },
 ];
 
